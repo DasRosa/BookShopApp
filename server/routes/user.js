09f1edc6c686fd4f7fd691fe5929c4c132d0955e
gundam22112001 <gundam22112001@gmai.com>
2021-08-18 23:15:48 +0700

    update getCart

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,13 +1,12 @@
 const express = require('express')
 const router = express.Router()
 
-const {getLogOut} = require('../controllers/auth')
+
 const { addItem,getAllUser,deleteItem,getCart,getUserName} = require('../controllers/user')
 
 router.route('/').get(getUserName)
 router.route('/:id').post(addItem).delete(deleteItem)
-// router.route('/admin').get(getAllUser)
+router.route('/admin').get(getAllUser)
 router.route('/cart').get(getCart)
-router.route('/logout').get(getLogOut)
 
 module.exports = router
